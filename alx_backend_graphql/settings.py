fcf1eac2684fdb128bf9f6e86127e69ce5cfdf5c
Musawenkosistar <musawenkosi2504@gmail.com>
2026-01-29 22:37:59 -0800

    alx-backend-graphql

--- alx_backend_graphql/settings.py
+++ alx_backend_graphql/settings.py
@@ -52,7 +52,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'alx_backend_graphql_crm.urls'
+ROOT_URLCONF = 'alx_backend_graphql.urls'
 
 TEMPLATES = [
     {
