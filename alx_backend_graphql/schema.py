be173c3ce4e45708d9272bc84d2d80afb66c53be
Musawenkosistar <musawenkosi2504@gmail.com>
2026-01-29 22:14:44 -0800

    Fix Query inheritance to satisfy checker

--- alx_backend_graphql/schema.py
+++ alx_backend_graphql/schema.py
@@ -1,6 +1,15 @@
 import graphene
 
-class Query(graphene.ObjectType):
-    hello = graphene.String(default_value="Hello, GraphQL!")
+
+class CRMQuery(graphene.ObjectType):
+    hello = graphene.String()
+
+    def resolve_hello(root, info):
+        return "Hello, GraphQL!"
+
+
+class Query(CRMQuery, graphene.ObjectType):
+    pass
+
 
 schema = graphene.Schema(query=Query)
